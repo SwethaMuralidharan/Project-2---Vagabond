7cb13bcb577035eaec0ac40e879aa975e96dc12c
GA LOANER 2 <lenstesterguy@gmail.com>
2018-01-12 15:07:03 -0800

    added routes for sign up, login completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :posts,dependent: :destroy
   has_many :cities,through: :posts
+  has_secure_password
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+  end
 end
